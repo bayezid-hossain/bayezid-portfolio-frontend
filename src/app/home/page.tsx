3c72ea3a109a47bdadb005989f6d7bcc040af55f
forsaken <forsaken@shadow.lan>
2023-08-24 11:14:43 -0400

    added home page added: particles added: avatar added: button

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 import TypeWriterText from '@/components/BlinkingText/TypeWriterText';
+import Image from 'next/image';
 
+import ParticlesContainer from '@/components/ParticlesContainer';
+import ProjectsBtn from '@/components/ProjectsBtn';
+import Avatar from '@/components/Avatar';
+import { fadeIn } from '../../../variants';
 import Transition from '@/components/Transition';
 import { useRouter } from 'next/navigation';
 import { AnimatePresence, motion } from 'framer-motion';
@@ -10,7 +15,73 @@ const HomePage = () => {
     <AnimatePresence mode="wait">
       <motion.div key={'home'} className="h-full">
         <Transition />
-        Home
+        <div className="bg-primary/60 h-full">
+          {/*text*/}
+          <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+            <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+              {/*title*/}
+              <motion.h1
+                variants={fadeIn('down', 0.2)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="h1"
+              >
+                Bringing Concepts <br /> Into{' '}
+                <span className="text-accent">Reality</span>
+              </motion.h1>
+              {/*subtitle*/}
+              <motion.p
+                variants={fadeIn('down', 0.3)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+              >
+                lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+                mostrum quam reprehendrit vero, tenetur voluptatem nulla aut
+                aspernatur dolores ut.
+              </motion.p>
+              {/*btn*/}
+              <motion.div
+                variants={fadeIn('down', 0.4)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="flex justify-center xl:hidden relative"
+              >
+                <ProjectsBtn />
+              </motion.div>
+              <motion.div
+                variants={fadeIn('down', 0.4)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="hidden xl:flex"
+              >
+                <ProjectsBtn />
+              </motion.div>
+            </div>
+          </div>
+          {/*image*/}
+          <div className="w-[1200px] h-full absolute right-0 bottom-0 ">
+            {/*bg img*/}
+            <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+          </div>
+          {/*particles*/}
+          <ParticlesContainer />
+          {/*Avatar img*/}
+          <motion.div
+            variants={fadeIn('up', 0.5)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            className="w-full h-full max-w-[737px] max-h-[678px] absolute bottom-32 lg:bottom-0 lg:right-[8%]"
+          >
+            <Avatar />
+          </motion.div>
+        </div>
       </motion.div>
     </AnimatePresence>
   );
